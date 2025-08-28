e8c8199deb472a8c0b541d392a916748df7e79e5
projectsofmohan <learningsofmohan@gmail.com>
2025-08-28 13:06:26 +0530

    extent report implementation

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,14 +2,34 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 	
 	protected WebDriver driver;
+	protected static ExtentReports extent;
+	protected ExtentTest test;
+	
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+	
+	@AfterSuite
+	public void tearDownReport() {
+		extent.flush();
+	}
 	
 	@BeforeMethod
 	public void setUp() {
@@ -21,7 +41,14 @@ public class BaseTest {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+		if(result.getStatus()==ITestResult.FAILURE) {
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "Login Failure");
+			System.out.println("Screenshot captured path: "+screenshotPath);
+			test.fail("Test Failed. Screenshot attached.", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+		
 		if(driver!=null) {
 			Log.info("Closing browser...");
 			driver.quit();
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,20 +5,62 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest{
 
 	@Test
-	public void testValidLogin() {
+	public void positiveLogInTest() {
 		Log.info("Starting login test ...");
+		test = ExtentReportManager.createTest("Positive LogIn test"); 
 		LoginPage loginPage = new LoginPage(driver);
+		test.info("Navigated to Login Page");
 		loginPage.enterUsername("student");
 		loginPage.enterPassword("Password123");
 		loginPage.clickSubmitButton();
+		test.info("Entered login credentials and clicked on Submit button");
 		System.out.println("Title : "+driver.getTitle());
 		Log.info("Verifying page title...");
+		test.info("Verifying page title");
 		Assert.assertEquals(driver.getTitle(), "Logged In Successfully | Practice Test Automation");
 		Log.info("Login test completed.");
+		test.pass("Login successful");
+	}
+	
+	@Test
+	public void negativeUsernameTest() {
+		Log.info("Starting login test ...");
+		test = ExtentReportManager.createTest("Negative username test"); 
+		LoginPage loginPage = new LoginPage(driver);
+		test.info("Navigated to Login Page");
+		loginPage.enterUsername("incorrectUser");
+		loginPage.enterPassword("Password123");
+		loginPage.clickSubmitButton();
+		test.info("Entered login credentials and clicked on Submit button");
+		System.out.println("Title : "+driver.getTitle());
+		Log.info("Verifying page title...");
+		test.info("Verifying page title");
+		Assert.assertEquals(driver.getTitle(), "Logged In Successfully | Practice Test Automation");
+		Log.info("Login test completed.");
+		test.pass("Login successful");
+	}
+	
+	@Test
+	public void negativePasswordTest() {
+		Log.info("Starting login test ...");
+		test = ExtentReportManager.createTest("Negative password test"); 
+		LoginPage loginPage = new LoginPage(driver);
+		test.info("Navigated to Login Page");
+		loginPage.enterUsername("student");
+		loginPage.enterPassword("incorrectPassword");
+		loginPage.clickSubmitButton();
+		test.info("Entered login credentials and clicked on Submit button");
+		System.out.println("Title : "+driver.getTitle());
+		Log.info("Verifying page title...");
+		test.info("Verifying page title");
+		Assert.assertEquals(driver.getTitle(), "Logged In Successfully | Practice Test Automation");
+		Log.info("Login test completed.");
+		test.pass("Login successful");
 	}
 }
